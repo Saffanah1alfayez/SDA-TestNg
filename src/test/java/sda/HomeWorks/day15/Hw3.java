1fbbab71a492b3243f6f9b754de93e1277ba48b2
safanh <safanhothman@gmail.com>
2024-03-13 14:21:59 +0300

    hw3

--- src/test/java/sda/HomeWorks/day15/Hw3.java
+++ src/test/java/sda/HomeWorks/day15/Hw3.java
@@ -1,4 +1,47 @@
 package sda.HomeWorks.day15;
 
-public class Hw3 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import sda.utilities.TestBase;
+
+public class Hw3 extends TestBase {
+    /*
+    Test Case3: Negative Password Test
+    Open page https://practicetestautomation.com/practice-test-login/
+    Type username student into Username field.
+    Type password incorrectPassword into Password field.
+    Puch Submit button.
+    Verify error message is displayed.
+    Verify error message text is Your password is invalid!
+     */
+
+    @Test
+    public void test() throws InterruptedException {
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+
+        WebElement username = driver.findElement(By.id("username"));
+        username.click();
+        username.sendKeys("student");
+
+        WebElement pass = driver.findElement(By.id("password"));
+        pass.click();
+        pass.sendKeys("Pass123");
+
+        WebElement sub = driver.findElement(By.id("submit"));
+        sub.click();
+
+        //Verify error message is displayed.
+        Thread.sleep(5000);
+
+        WebElement eM = driver.findElement(By.id("error"));
+        SoftAssert sa = new SoftAssert();
+        sa.assertTrue(eM.isDisplayed());
+
+        //Verify error message text is Your password is invalid!
+        sa.assertTrue(eM.getText().contains("Your password is invalid!"));
+
+        sa.assertAll();
+    }
 }
